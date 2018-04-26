609de7a04a3ae9d90e6eeee2f07ce65edfec2d13
EC2 Default User <ec2-user@ip-172-31-25-152.eu-central-1.compute.internal>
2018-04-26 15:12:04 +0000

    Added html

--- web/app.py
+++ web/app.py
@@ -1,10 +1,17 @@
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "here will be my upload interface"
+    return render_template (
+     'upload_files.html'
+    )
+@app.route("/upload")
+def handle_upload():
+  return OK
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000, debug=True)
+  app.run(host="0.0.0.0", port=8080, debug=True)
