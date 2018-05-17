25d6eaa8eb55d6111c7254cba87f4937415ce1d4
Nikolya Shchur <kolya_shchur@mail.ru>
2018-05-17 15:56:13 +0000

    oders added

--- tests/test_name_generator.py
+++ tests/test_name_generator.py
@@ -3,9 +3,9 @@ from media.name_generator import generate_name
 
 class TestNameGenerator(unittest.TestCase):
 
-  def test_generation_of_unique_name():
+  def test_generation_of_unique_name(self):
     n1 = generate_name('epsy.jpg')
     n2 = generate_name('epsy.jpg')
-    regexp = re.compile(r'^uploads\/[0-9a-f-](32)\/epsy\.jpg$')
+    regexp = re.compile(r'^uploads\/[0-9a-f-](36)\/epsy\.jpg$')
     assert n1 != n2
     assert regexp.search(n1)
--- web/app.py
+++ web/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask import render_template, request, flash
 from media.s3_storage import S3MediaStorage
+from media.name_generator import 
 
 app = Flask(__name__)
 
@@ -21,18 +22,25 @@ def handle_upload():
         flash('No file part')
         return redirect(request.url)
     uploaded_file = request.files['uploaded_file']
+    file_ref = generate_name(uploaded_file.filename)
     media_storage.store (
-        dest="/uploaded/%s" % uploaded_file.filename,
+        dest=file_ref,
         source=uploaded_file
     )
+    orders.load(current_user()).add_file(file_ref)
     return "OK"
+
+@app.route(/"proceed")
+def proceed():
+  order = orders.load(current_user())
+  handler.handle(order,anapshot())
 
 @app.route("/make-animation")
 def make_animation():
-    return render_template(
-      "make_animation.html",
-      invitation="only limit is yourself"
-    )
+  return render_template(
+    "make_animation.html",
+    invitation="only limit is yourself"
+  )
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
